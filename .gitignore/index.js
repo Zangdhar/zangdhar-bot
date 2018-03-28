32ecc0cf24a9194ac843f6499530eef24998128a
Zangdhar <37810540+Zangdhar@users.noreply.github.com>
2018-03-28 10:51:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,25 +8,3 @@ console.log("Zangdhar est connecté");
 });
 
 bot.login(process.env.TOKEN);
-
-var prefix = ("*")
-
-bot.on("message", message => {
-  if (message.content === prefix + "help"){
-    message.channel.sendMessage("Liste des commandes: /n -*help");
-  }
-  
-  if (message.content === "salut"){
-   message.reply("Bonjour ! ça va ?"); 
- console.log("Commande Salut Effectué");
-  });
-  
-  if (message.content === "oui et toi ?"){
-   message.reply("ça va nickel merci ^^"); 
-  console.log("Commande oui et toi Effectué");
-  });
-  
-  if (message.content === "pd"){
-   message.reply("Sueveilles ton language ou je te Ban Def !");
-  console.log("Commmade Avertissement Language Effectué");
-  });
