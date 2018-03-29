83dea107d496b2d2a63abaac7ae2a7705eff582f
Zangdhar <37810540+Zangdhar@users.noreply.github.com>
2018-03-29 12:03:05 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready" , function() {
-  bot.user.setGame("Respecte ma p*tain d'autorité !");
+  bot.user.setGame("Double Toz");
 console.log("Zangdhar est connecté");
 });
 
