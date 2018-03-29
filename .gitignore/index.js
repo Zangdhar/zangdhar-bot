7068845cc4ac8ecee6d10e971fae45ad8bd2e233
Zangdhar <37810540+Zangdhar@users.noreply.github.com>
2018-03-29 11:30:13 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,32 +2,24 @@ const Discord = require("discord.js");
 
 var bot = new Discord.Client();
 
-var prefix = ("*")
-
 bot.on("ready" , function() {
   bot.user.setGame("Respecte ma p*tain d'autorité !");
 console.log("Zangdhar est connecté");
 });
 
 bot.login(process.env.TOKEN);
+var prefix = (":::");
 
 
-bot.on('message', message => {
-  if (message.content === prefix + "help"){
-    message.channel.sendMessage("Liste des commandes: /n -*help");
-  }
-  
-  if (message.content === "salut"){
-   message.reply("Bonjour ! ça va ?"); 
- console.log("Commande Salut Effectué");
-  });
-  
-  if (message.content === "oui et toi ?"){
-   message.reply("ça va nickel merci ^^"); 
-  console.log("Commande oui et toi Effectué");
-  });
-  
-  if (message.content === "pd"){
-   message.reply("Sueveilles ton language ou je te Ban Def !");
-  console.log("Commmade Avertissement Language Effectué");
-  });
+bot.on("guildMemberAdd", member=> {
+member.guild.channels.find("name", "general").send('Bienvenue ${member}')
+})
+
+bot.on("guildMemberRemove", member => {
+member.guild.channels.find("name", "general").send('${member} viens de quitter')
+})
+
+bot.on('guildMemberAdd', member => {
+var role = member.guild.roles.find('name', 'Membres');
+member.addRole(role)
+})
